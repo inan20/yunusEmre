7569bd0d9efc6dd52a953b3d9b043ddef04247cb
Ahmet İnan <ahmetinanyunefa@gmail.com>
2022-11-18 12:07:40 +0300

    yunusEmre

---
+++ src/day02_variables/C01.java
@@ -0,0 +1,17 @@
+package day02_variables;
+
+public class C01 {
+    public static void main(String[] args) {
+        String yunus="selamın aleyküm";
+        System.out.println(yunus);
+        int not=65;
+        int not2=80;
+        int ort=(not+not2)/2;
+
+        System.out.println(ort);
+
+
+
+
+    }
+    }
